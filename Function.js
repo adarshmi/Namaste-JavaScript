f0eba9296237520a38bce837fa40da3ddeecbf57
adarshmi <adarshmi1234@gmail.com>
2023-03-31 17:36:46 +0530

    newa

--- Function.js
+++ Function.js
@@ -1,10 +1,11 @@
 var x  = 1;
 a();
-b();
+b();  // we are calling the functions before defining them. This will work properly, as seen in Hoisting.
+
 console.log(x);
 
 function a(){
-    var x = 10;
+    var x = 10;// local scope because of separate execution context
     console.log(x);
 }
 
@@ -12,3 +13,8 @@ function b(){
     var x =100;
     console.log(x);
 }
+
+// Output
+// 10
+// 100
+// 1
